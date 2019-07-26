44d049d66cb9605e67baa27db3398314b54da017
unknown <gokulramalingam1@gmail.com>
2019-07-26 17:35:46 +0530

    Simple Angular Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
+import {PlayerService} from './player.service';
+import { PlaygroundComponent } from './playground/playground.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { PlaywinnerComponent } from './playwinner/playwinner.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayersComponent,
+    PlaygroundComponent,
+    PlaywinnerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ PlayerService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
